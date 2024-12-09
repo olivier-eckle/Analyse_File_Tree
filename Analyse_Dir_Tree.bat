cc0e25bb55ba700c68f75af8dc8781ce543bb42b
olivier ECKLE <164765047+olivier-eckle@users.noreply.github.com>
2024-12-09 13:09:05 +0100

    Update Analyse_Dir_Tree.bat

--- Analyse_Dir_Tree.bat
+++ Analyse_Dir_Tree.bat
@@ -4,7 +4,7 @@ REM Script batch pour exécuter Analyse_File_Tree.py
 REM Spécifiez le chemin de Python si nécessaire (par exemple, si Python n'est pas dans le PATH).
 SET PYTHON_EXECUTABLE=python
 REM Spécifiez le chemin relatif ou complet de votre script Python.
-SET SCRIPT_PATH=.\Analyse_File_Tree.py
+SET SCRIPT_PATH=.\Creation_JSON_Dir_Tree.py
 echo %PYTHON_EXECUTABLE% %SCRIPT_PATH%
 
 REM Lance le script Python
